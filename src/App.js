24bcaab19e8a5ae2d69ec61a2ba790eef6d280ff
Andy-Aseye <andyaseye@gmail.com>
2023-01-31 14:14:23 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import { Upload } from "@aws-sdk/lib-storage";
+import { S3Client, S3 } from "@aws-sdk/client-s3";
+
 
 function App() {
+
+  const upload = (file) => {
+    
+    var file = file.target.files[0];
+
+    const target= { Bucket:"andybucket12", Key:file.name, Body:file };
+    const creds = {accessKeyId: process.env.REACT_APP_AWS_ACCESS_KEY, secretAccessKey: process.env.REACT_APP_AWS_SECRET_KEY}
+    
+try {
+  const parallelUploads3 = new Upload({
+    client: new S3Client({region:"awsregion", credentials: creds}),
+    leavePartsOnError: false, // optional manually handle dropped parts
+    params: target,
+  });
+
+  parallelUploads3.on("httpUploadProgress", (progress) => {
+    console.log(progress);
+  });
+
+   parallelUploads3.done();
+}
+
+
+catch (e) {
+  console.log(e);
+}
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <form onClick={() => document.querySelector(".input-field").click()}>
+    <input type="file" onChange={upload} className="input-field" />
+    </form>
+    </>
   );
 }
 
 export default App;
+
+
+
+
+
